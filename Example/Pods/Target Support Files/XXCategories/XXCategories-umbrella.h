8ab1382291a20df6d5ef9f250398b340ef9e97ae
xiongbingyang <595919268@qq.com>
2017-07-15 17:17:41 +0800

    添加nsdata aes加密的分类；

--- Example/Pods/Target Support Files/XXCategories/XXCategories-umbrella.h
+++ Example/Pods/Target Support Files/XXCategories/XXCategories-umbrella.h
@@ -2,6 +2,11 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "NSArray+ToString.h"
+#import "NSData+Encryption.h"
+#import "NSDictionary+ToString.h"
+#import "NSString+Base64.h"
+#import "NSString+FilePath.h"
 
 FOUNDATION_EXPORT double XXCategoriesVersionNumber;
 FOUNDATION_EXPORT const unsigned char XXCategoriesVersionString[];
--- XXCategories.podspec
+++ XXCategories.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'XXCategories'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'XXCategories has many categories'
 
 # This description is used to generate tags and improve search results.
